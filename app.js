7abaf9ea366a66774d4d7f0998d339e56f86b2b9
malkamat <rimka.matkalo@gmail.com>
2020-06-15 18:54:11 +0200

    ajout des jours de la semaine dynamiquement avec l'objet Date

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ const localisation = document.querySelector(".localisation")
 const temperature = document.querySelector(".temperature")
 const description = document.querySelector(".description")
 const canvas = document.querySelector("#icone")
+const joursSemaine = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]
+const joursDiv = document.querySelectorAll(".joursSemaine")
+const iconePrev = document.querySelectorAll(".iconePrevision")
+
+//date
+
+let newDate =  new Date()
+// console.log(newDate)
+let options = {weekday: "long"}
+let jourActuel = newDate.toLocaleDateString("fr-FR", options)
+// console.log(jourActuel)
+jourActuel = jourActuel.charAt(0).toUpperCase() + jourActuel.slice(1)
+// console.log(jourActuel)
+
+// calcul les 7 jours de la semaine fonction de la date actuel
+
+let tabJoursOrdre = joursSemaine.slice(joursSemaine.indexOf(jourActuel)).concat(joursSemaine.slice(0, joursSemaine.indexOf(jourActuel)))
+console.log(tabJoursOrdre)
+
+for(i = 0; i < tabJoursOrdre.length; i++) {
+  joursDiv[i].innerHTML = tabJoursOrdre[i].slice(0,3)
+}
+
 
 //event "load" qui récupere la longitude et la latitude pour les intégrer à l'appel de l'api
 window.addEventListener("load", () => {
@@ -26,6 +49,10 @@ window.addEventListener("load", () => {
          temperature.textContent =  `${Math.trunc(data.current.temp)} °`
          description.textContent = data.current.weather[0].main
          canvas.innerHTML = `<img src=" http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png">`
+
+         for(n =0; n < 7; n++) {
+           
+         }
 
         } else {
           console.log(response.status)
